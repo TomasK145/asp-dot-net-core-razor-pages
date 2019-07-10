1ceb01c60990588cfb5e6df8bb2d5038e7d0a509
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-07-10 14:50:43 +0200

    5. Working with data - pouzitie tag helperu, vyuzitie OnGet metody, bindovanie hodnot z fieldu do properties objektu, custom page handlers (Delete handler + asp-page-handler vramci page), upload image

--- TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
+++ TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TopsyTurvyCakes.Models;
@@ -19,10 +21,54 @@ namespace TopsyTurvyCakes.Pages.Admin
                 return Id == null;
             }
         }
+
+        [BindProperty] //vyuzitie model bindingu --> field name vo forme a property name sa musia zhodovat --> vyuzitie Tag helperu zabezpecilo zachovanie spravnych nazvov fieldov
         public Recipe Recipe { get; set; }
 
-        public void OnGet()
+        [BindProperty]
+        public IFormFile Image { get; set; }
+
+        private readonly IRecipesService _recipesService;
+
+        public AddEditRecipeModel(IRecipesService recipesService)
         {
+            _recipesService = recipesService;
+        }
+
+        //public void OnGet() --> mozne vyuzit async verziu
+        public async Task OnGetAsync() //nezov metody je suportovany: OnGet alebo OnGetAsync
+        {
+            Recipe = await _recipesService.FindAsync(Id.GetValueOrDefault()) ?? new Recipe();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var recipe = await _recipesService.FindAsync(Id.GetValueOrDefault()) ?? new Recipe();
+
+            recipe.Name = Recipe.Name;
+            recipe.Description = Recipe.Description;
+            recipe.Ingredients = Recipe.Ingredients;
+            recipe.Directions = Recipe.Directions;
+
+            if (Image != null)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    await Image.CopyToAsync(stream);
+
+                    recipe.Image = stream.ToArray();
+                    recipe.ImageContentType = Image.ContentType;
+                }
+            }
+
+            await _recipesService.SaveAsync(recipe);
+            return RedirectToPage("/Recipe", new { id = recipe.Id });
+        }
+
+        public async Task<IActionResult> OnPostDelete() //metoda (handler) volana pri DELETE HTTP verb
+        {
+            await _recipesService.DeleteAsync(Id.Value);
+            return RedirectToPage("/Index");
         }
     }
 }
